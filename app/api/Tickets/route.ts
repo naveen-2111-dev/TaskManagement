3538629a3c51518361d4e30dc36c174b9a84fac8
naveen Rajan <sce22cs026@sairamtap.edu.in>
2024-05-18 00:03:07 +0530

    api update

--- app/api/Tickets/route.ts
+++ app/api/Tickets/route.ts
@@ -1,11 +1,12 @@
-import Ticket from '../(models)/Ticket';
+import Ticket from '../../(models)/Ticket';
 import { NextResponse } from 'next/server';
 import { props } from '@/types';
 
 export async function POST({ req }: props) {
+  console.log("res", req.body);
   try {
     const body = await req.json();
-    const ticketdata = body.formdata;
+    const ticketdata = body.form;
     await Ticket.create(ticketdata);
 
     return NextResponse.json({ message: 'ticket created!' }, { status: 201 });
